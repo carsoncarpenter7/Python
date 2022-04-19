524a44b599e8a56963544614047f96b284e786dc
carsoncarpenter7 <42815820+carsoncarpenter7@users.noreply.github.com>
2022-04-19 16:51:30 -0700

    Delete Lab 8 directory

--- Lab 8/part-2/urlshortener/config.py
+++
@@ -1,6 +0,0 @@
-import os
-
-class Config(object):
-    SECRET_KEY = os.environ.get("FLASK_SECRET_KEY") or "mysupersecretkey"
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL") or "sqlite:///urlshortener.sqlite3"
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
