367017707fedd8f27b136e37928da342692f96d6
carsoncarpenter7 <42815820+carsoncarpenter7@users.noreply.github.com>
2022-04-19 16:52:59 -0700

    Add files via upload

---
+++ Lab 8/part-2/urlshortener/config.py
@@ -0,0 +1,6 @@
+import os
+
+class Config(object):
+    SECRET_KEY = os.environ.get("FLASK_SECRET_KEY") or "mysupersecretkey"
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL") or "sqlite:///urlshortener.sqlite3"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
